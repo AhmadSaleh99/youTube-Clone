00afa67d11ddb13e9bddffaadfd395645a0f38ca
AhmadSaleh99 <saleehsale816@gmail.com>
2023-01-08 22:15:18 +0530

    commiting

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/Constants";
 
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
-};
+const ChannelCard = ({ channelDetails }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "356px", md: "320px" },
+      height: "326px",
+      margin: "auto",
+    }}
+  >
+    <Link to={`/channel/${channelDetails?.id?.channelId}`}>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "#fff",
+        }}
+      >
+        <CardMedia
+          image={
+            channelDetails?.snippet?.thumbnails?.high?.url || demoProfilePicture
+          }
+          alt={channelDetails?.snippet?.title}
+          sx={{
+            borderRadius: "50%",
+            height: "180px",
+            width: "180px",
+            mb: 2,
+            border: "1px solid #e3e3e3",
+          }}
+        />
+
+        <Typography variant="h6">
+          {channelDetails?.snippet?.title}
+          <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
+        </Typography>
+
+        {channelDetails?.statistics?.subscriberCount && (
+          <Typography>
+            {parseInt(
+              channelDetails?.statistics?.subscriberCount
+            ).toLocaleString()}{" "}
+            Subscribers
+          </Typography>
+        )}
+      </CardContent>
+    </Link>
+  </Box>
+);
 
 export default ChannelCard;
