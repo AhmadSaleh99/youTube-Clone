01f11ad70707d2264159dfefaedf342ddfb7c8aa
AhmadSaleh99 <saleehsale816@gmail.com>
2023-01-11 02:26:24 +0530

    commiting

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,12 +1,21 @@
 import { Stack, Box } from "@mui/system";
 import { VideoCard, ChannelCard } from "./";
-const Videos = ({ videos }) => {
+
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return "Loading...";
+
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
-          {item.id.channelId && <ChannelCard channelDetails={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
